8d18fb377e0bb0e38ea73fc877cad4ff3d3e2594
Kahno <pahor.samo@gmail.com>
2020-09-18 16:29:20 +0200

    added card recommending functionality

--- analyze.py
+++ analyze.py
@@ -2,6 +2,7 @@ import json
 
 
 MASTER_JSON_FILE = "cedh_decklists.json"
+SCRYFALL_CARD_DICTIONARY = "scryfall_card_dictionary.json"
 
 BASIC_LANDS = [
     "Plains",
@@ -18,18 +19,54 @@ BASIC_LANDS = [
 ]
 
 def load_database():
+    """
+    Loads ddb from json file
+    """
+
     with open(MASTER_JSON_FILE, "r") as f:
         data = json.loads(f.read())
 
     return data
 
 
+def load_scryfall():
+    """
+    Loads scryfall data from json file
+    """
+
+    with open(SCRYFALL_CARD_DICTIONARY, "r") as f:
+        scryfall_card_dict = json.loads(f.read())
+
+    return scryfall_card_dict
+
+
+def flatten(data):
+    """
+    Flattens database master json file for easier access
+    """
+
+    flat_data = dict()
+
+    for color in data:
+        for deck_type in data[color]:
+            for deck in data[color][deck_type]:
+                flat_data[deck] = data[color][deck_type][deck]
+
+    return flat_data
+
+
 def find_decklist(dataset, target_name):
+    """
+    Finds desired decklist via direct name matching
+    """
+
     for color in dataset:
         for deck_type in dataset[color]:
             for deck_name in dataset[color][deck_type]:
                 if deck_name == target_name:
                     return dataset[color][deck_type][deck_name]
+    print("Deck not found!")
+    return []
 
 
 def jaccard_similarity(a, b):
@@ -41,6 +78,10 @@ def jaccard_similarity(a, b):
 
 
 def not_basic_land(card):
+    """
+    Checks if a given card is not one of the basic lands
+    """
+
     return card not in BASIC_LANDS
 
 
@@ -58,7 +99,84 @@ def deck_similarity(dataset, deck_1, deck_2):
     return jaccard_similarity(decklist_1, decklist_2)
 
 
+def deck_rep_by_color(dataset):
+    """
+    Constructs frequency dictionary of decks on the ddb by color identity
+    """
+
+    color_rep = {}
+
+    for color in dataset:
+        cur_rep = 0
+
+        for deck_type in dataset[color]:
+            for deck in dataset[color][deck_type]:
+                cur_rep += 1
+
+        color_rep[color] = cur_rep
+
+    return color_rep
+
+
+def max_inclusion_value(card, ddb_color_rep, scryfall_card_dict):
+    """
+    Computes the total number of possible decks which are
+    able to include a given card based on color identity
+    """
+
+    card_identity = scryfall_card_dict[card]["color_identity"]
+
+    max_inclusion = 0
+    is_colorless = not card_identity
+
+    for color_combo in ddb_color_rep:
+        if all(c in color_combo.upper() for c in card_identity) or is_colorless:
+            max_inclusion += ddb_color_rep[color_combo]
+
+    return max_inclusion
+
+
+def deck_identity(decklist, scryfall_card_dict):
+    """
+    Computes color identity of a given deck
+    """
+
+    deck_colors = set()
+
+    for card in decklist:
+        for color in scryfall_card_dict[card]["color_identity"]:
+            deck_colors.add(color)
+
+    return list(deck_colors)
+
+
+def ci_filter(ci, scryfall_card_dict):
+    """
+    Returns a function that filters a given card based on provided color identity
+    """
+    s = scryfall_card_dict
+
+    return lambda x: (not s[x]["color_identity"]) or all(c in ci for c in s[x]["color_identity"])
+
+
+def decklist_filter(decklist):
+    """
+    Returns boolean function that check if input card is found
+    in a given decklist
+    """
+
+    return lambda x: x not in decklist
+
+
 def summary(dataset, output=False):
+    """
+    Constructs:
+        - frequency dictionary of all cards on ddb
+        - number of decks on ddb
+
+    and shows basic card summary
+    """
+
     all_cards = {}
     num_decks = 0
 
@@ -71,13 +189,6 @@ def summary(dataset, output=False):
                     all_cards[card] += 1
                 num_decks += 1
 
-    for card in sorted(filter(not_basic_land, all_cards.keys()), key=lambda x: all_cards[x]):
-        if output:
-            print(f"{all_cards[card] / num_decks:.4f} ({all_cards[card]}/{num_decks}) {card}")
-
-    if output:
-        print(f"\nTotal number of unique cards on database is {len(all_cards.keys())}\n")
-
     for i in range(1, 11):
         cur_representation = 0
         for card in all_cards:
@@ -89,7 +200,59 @@ def summary(dataset, output=False):
     return all_cards, num_decks
 
 
-if __name__ == "__main__":
-    dataset = load_database()
+def max_inclusion_ratio(card, all_ddb_cards, ddb_color_rep, scryfall_card_dict):
+    """
+    Computes the database representation of a given card weighted
+    by its maximum possible inclusion
+    """
 
-    summary(dataset)
+    max_inclusion = max_inclusion_value(card, ddb_color_rep, scryfall_card_dict)
+
+    return all_ddb_cards[card] / max_inclusion
+
+
+def recommend_cards(decklist):
+    dataset = load_database()
+    scryfall_card_dict = load_scryfall()
+
+    flat_dataset = flatten(dataset)
+    all_cards, num_decks = summary(dataset)
+    ddb_color_rep = deck_rep_by_color(dataset)
+
+    mi_val = lambda x: max_inclusion_value(x, ddb_color_rep, scryfall_card_dict)
+    mi_ratio = lambda x: max_inclusion_ratio(x, all_cards, ddb_color_rep, scryfall_card_dict)
+    rep_ratio = lambda x: all_cards[x] / num_decks
+    combo = lambda x: 0.5 * mi_ratio(x) + 0.5 * rep_ratio(x)
+
+    # Color identity filter
+    cif = ci_filter(deck_identity(decklist, scryfall_card_dict), scryfall_card_dict)
+
+    # Card filter
+    dif = decklist_filter(decklist)
+
+    # Composite filter
+    cf = lambda x: cif(x) and dif(x) and not_basic_land(x)
+
+    print(f"Total number of decks on ddb: {num_decks}")
+    print("MI - ratio of decks with card versus all decks in legal color identity")
+    print("RP - ratio of decks with card versus all decks on the ddb")
+    print()
+
+    for i, card in enumerate(sorted(filter(cf, all_cards.keys()), key=combo, reverse=True)[:50]):
+        print(
+            f"{i+1}. {card} ({all_cards[card]}/{mi_val(card)}) "
+            f"(MI: {mi_ratio(card):.3f}) "
+            f"(RP: {rep_ratio(card):.3f})"
+        )
+
+
+if __name__ == "__main__":
+
+    # Json data pulled from deckbox used for testing
+    test_deck = []
+    with open("test_deck.json", "r") as f:
+        data = json.loads(f.read())
+        for i, x in enumerate(data):
+            test_deck.append(data[x]["name"])
+
+    recommend_cards(test_deck)
