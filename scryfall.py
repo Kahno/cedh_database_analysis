c8538508844b120a61c0febe639fe7c64848a04e
Kahno <pahor.samo@gmail.com>
2020-09-18 16:11:16 +0200

    ignoring scryfall json file and code cleanup

--- scryfall.py
+++ scryfall.py
@@ -12,18 +12,11 @@ if __name__ == "__main__":
     all_cards, num_decks = summary(dataset)
     card_dictionary = {}
 
-    # loop over all found cards on DDB
+    # loop over all found cards on DDB and add their info to master json
     for card in all_cards.keys():
-
         print(f"Fetching card: {card}")
-
-        # perform scryfall api call
         response = requests.get(f"https://api.scryfall.com/cards/named?exact={card}")
-
-        # save json data to master json
         card_dictionary[card] = response.json()
-
-        # wait 0.5 seconds
         time.sleep(0.5)
 
     with open("card_dictionary.json", "w") as f:
