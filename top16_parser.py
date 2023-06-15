391aa12d026a888ba894326e13695cf05701b09d
Egej Vencelj <egej.vencelj@gmail.com>
2023-06-15 22:46:13 +0200

    *Update: WIP parser

--- top16_parser.py
+++ top16_parser.py
@@ -1,15 +1,28 @@
 import argparse
 import datetime
+
+from pydantic import Json
 from scraper import *
 import json
 import requests
 from enum import Enum
 from datetime import datetime, date
+import urllib.parse
 
 
 base_url = "https://edhtop16.com/api/"
 headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
 
+# Custom validation function for color combinations
+def validate_color_combination(colors):
+    valid_colors = ['W', 'U', 'B', 'R', 'G', 'C']
+    if 'C' in colors and len(colors) > 1:
+        raise argparse.ArgumentTypeError("Colorless (C) cannot be combined with other colors")
+
+    for color in colors:
+        if color not in valid_colors:
+            raise argparse.ArgumentTypeError(f"Invalid color: {color}")
+    return list("".join(colors))
 
 class JSONObject:
     def __init__(self, **kwargs):
@@ -26,12 +39,13 @@ class JSONObject:
         }
 
     def __repr__(self):
-        return '\n'.join(
+        '''return '\n'.join(
             '{}:{}'.format(
                 str(k), str(v) if not isinstance(v, JSONObject) else repr(v).replace('\n', '\n\t')
             )
             for k,v in self.__dict__.items()
-        )
+        )'''
+        return json.dumps(self.json, indent=4)
 
 RATE = 120/60 #[s]
 
@@ -43,17 +57,83 @@ DEFAULT_QUERY = {
     }
 }
 
-CMP = JSONObject(**{
-    'LTE': '$lte',
-    'GTE': '$gte',
-    'EQ': '$eq',
-})
-
 def str2timestamp(s:str) -> int:
     '''
     Converts a string of the form DDMMYYYY to a unix timestamp
     '''
     return int(datetime.strptime(s, '%d%m%Y').timestamp())
+
+
+def generate_query(args):
+    data = {}
+    if args.tstanding is not None:
+        data['standing'] = {}
+        if args.tstanding.lte is not None:
+            data['standing']['$lte'] = args.tstanding.lte
+        if args.tstanding.gte is not None:
+            data['standing']['$gte'] = args.tstanding.gte
+
+    if args.tsize is not None or args.tdate is not None:
+        data['tourney_filter'] = {}
+        if args.tsize is not None:
+            data['tourney_filter']['size'] = {}
+            if args.tsize.lte is not None:
+                data['tourney_filter']['size']['$lte'] = args.tsize.lte
+            if args.tsize.gte is not None:
+                data['tourney_filter']['size']['$gte'] = args.tsize.gte
+        if args.tdate is not None:
+            data['tourney_filter']['dateCreated'] = {}
+            if args.tdate.lte is not None:
+                data['tourney_filter']['dateCreated']['$lte'] = args.tdate.lte
+            if args.tdate.gte is not None:
+                data['tourney_filter']['dateCreated']['$gte'] = args.tdate.gte
+    if args.tentries is not None:
+        data['entries'] = {}
+        if args.tentries.lte is not None:
+            data['entries']['$lte'] = args.tentries.lte
+        if args.tentries.gte is not None:
+            data['entries']['$gte'] = args.tentries.gte
+    if args.color is not None:
+        data['colorID'] = args.color
+    return JSONObject(**data)
+
+def get_entries(query):
+    data = json.loads(requests.post(base_url + 'req', json=query.json, headers=headers).text)
+    entries = [JSONObject(**entry) for entry in data]
+    unique_commanders = set(x.commander for x in entries)
+
+    return entries
+
+
+
+
+def decode_url_query(url):
+    '''
+    Decodes a url query into a dict
+    '''
+
+    URL  = urllib.parse.unquote(url)
+
+    query = {}
+    for key, value in urllib.parse.parse_qs(URL.split("?")[1]).items():
+        key = key.split("__")
+        if len(key) == 1:
+            query[key[0]] = value[0]
+        elif len(key) == 2:
+            if key[0] not in query:
+                query[key[0]] = {}
+            query[key[0]][key[1]] = value[0]
+        elif len(key) == 3:
+            if key[0] not in query:
+                query[key[0]] = {}
+            if key[1] not in query[key[0]]:
+                query[key[0]][key[1]] = {}
+            #TODO: Correct types
+            query[key[0]][key[1]][key[2]] = int(value[0]) if value[0].isdigit() else value[0]
+
+
+    return JSONObject(**query)
+
 
 '''
 tourney_filter: {
@@ -79,8 +159,15 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='EDH Top 16 Parser')
 
     # Define the "size" and "date" argument pairs
-    parser.add_argument('--size', dest='tsize', nargs='+', help='Size filter (optional: gte/lte followed by a number)')
-    parser.add_argument('--date', dest='tdate', nargs='+', help='Date filter (optional: gte/lte followed by a date)')
+    parser.add_argument('--size', dest='tsize', nargs='+', help='Size filter (optional: +gte/+lte followed by a number)')
+    parser.add_argument('--date', dest='tdate', nargs='+', help='Date filter (optional: +gte/+lte followed by a date)')
+    parser.add_argument('--standing', dest='tstanding', nargs='+', help='Standing filter (optional: +gte/+lte followed by a number)')
+    parser.add_argument('--entries', dest='tentries', nargs='+', help='Entries filter (optional: +gte/+lte followed by a number)')
+    # Define the '--color' argument with custom validation
+    parser.add_argument('--color', type=validate_color_combination,
+                        help='MTG color(s) (W - White, U - Blue, B - Black, R - Red, G - Green, C - Colorless)')
+
+
 
     # Parse the command-line arguments
     args = parser.parse_args()
@@ -88,42 +175,28 @@ if __name__ == '__main__':
     dtypes = {
         'tsize': int,
         'tdate': str2timestamp,
-        'standing': int,
+        'tstanding': int,
+        'tentries': int,
     }
     for arg in vars(args):
-        opt_args = getattr(args, arg)
-        if opt_args is not None:
-            option_parser = argparse.ArgumentParser(prefix_chars='+')
-            dtype = dtypes[arg]
+        if arg in dtypes:
+            opt_args = getattr(args, arg)
+            if opt_args is not None:
+                option_parser = argparse.ArgumentParser(prefix_chars='+')
+                dtype = dtypes[arg]
 
-            option_parser.add_argument('+lte', type=dtype, help='Maximum tournament size')
-            option_parser.add_argument('+gte', type=dtype, help='Minimum tournament size')
-            opt_args = option_parser.parse_args(getattr(args, arg))
+                option_parser.add_argument('+lte', type=dtype, help='Maximum tournament size')
+                option_parser.add_argument('+gte', type=dtype, help='Minimum tournament size')
+                opt_args = option_parser.parse_args(getattr(args, arg))
 
-            setattr(args, arg, opt_args)
+                setattr(args, arg, opt_args)
+    url = "https://edhtop16.com/?tourney_filter__size__%24gte=64&tourney_filter__dateCreated__%24gte=1672527600&standing__%24lte=16&entries__%24gte=20&colorID=null"
+    org_query = decode_url_query(url)
+    #print(org_query)
+    query = generate_query(args)
+    print(query)
+    entries = get_entries(org_query)
 
-
-
-
-    # Use the filter values as needed
-    # Perform further processing or actions based on the filters
-
-
-    '''size.add_argument('--gte', type=int, help='Minimum tournament size')
-    size.add_argument('--lte', type=int, help='Maximum tournament size')
-
-    #max/min/eq date
-    tdate = parser.add_argument_group('Tournament date')
-    tdate.add_argument('--gte', type=str2timestamp, help='Minimum tournament date')
-    tdate.add_argument('--lte', type=str2timestamp, help='Maximum tournament date')
-
-    standing = parser.add_argument_group('Placement')
-    standing.add_argument('--lte', type=int, help='Maximum placement')
-    standing.add_argument('--gte', type=int, help='Minimum placement')'''
-
-
-    # Parse the command-line arguments
-
-
+    #print(entries)
     pass
 
