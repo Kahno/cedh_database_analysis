ea45746970f42aed1718153866660fcc22232bd5
Egej Vencelj <egej.vencelj@gmail.com>
2023-06-15 14:54:19 +0200

    *Update: Argparse

--- top16_parser.py
+++ top16_parser.py
@@ -65,26 +65,65 @@ standing = {[$lte | $gte | $eq] : <N>}
 colorID = [(W | U | B | R | G) ^ C]
 '''
 
-data = JSONObject(**DEFAULT_QUERY)
+#data = JSONObject(**DEFAULT_QUERY)
 
-assert 1672527600 == str2timestamp('01012023')
-assert str2timestamp('02012023') - str2timestamp('01012023') == 24*60*60
-assert data.json == DEFAULT_QUERY
+#assert 1672527600 == str2timestamp('01012023')
+#assert str2timestamp('02012023') - str2timestamp('01012023') == 24*60*60
+#assert data.json == DEFAULT_QUERY
 
-entries = json.loads(requests.post(base_url + 'req', json=data.json, headers=headers).text)
-sprint(entries)
+#entries = json.loads(requests.post(base_url + 'req', json=data.json, headers=headers).text)
+#print(entries)
 
 if __name__ == '__main__':
     #parser
     parser = argparse.ArgumentParser(description='EDH Top 16 Parser')
-    #max/min/eq tournament size
-    size = parser.add_mutually_exclusive_group()
 
-    # Add the operation argument
-    size.add_argument('op', choices=['lte', 'gte', 'eq'], help='Tournament size operation')
-    # Add the number argument
-    size.add_argument('n', type=int, help='Number to compare')
+    # Define the "size" and "date" argument pairs
+    parser.add_argument('--size', dest='tsize', nargs='+', help='Size filter (optional: gte/lte followed by a number)')
+    parser.add_argument('--date', dest='tdate', nargs='+', help='Date filter (optional: gte/lte followed by a date)')
 
     # Parse the command-line arguments
     args = parser.parse_args()
+
+    dtypes = {
+        'tsize': int,
+        'tdate': str2timestamp,
+        'standing': int,
+    }
+    for arg in vars(args):
+        opt_args = getattr(args, arg)
+        if opt_args is not None:
+            option_parser = argparse.ArgumentParser(prefix_chars='+')
+            dtype = dtypes[arg]
+
+            option_parser.add_argument('+lte', type=dtype, help='Maximum tournament size')
+            option_parser.add_argument('+gte', type=dtype, help='Minimum tournament size')
+            opt_args = option_parser.parse_args(getattr(args, arg))
+
+            setattr(args, arg, opt_args)
+
+
+
+
+    # Use the filter values as needed
+    # Perform further processing or actions based on the filters
+
+
+    '''size.add_argument('--gte', type=int, help='Minimum tournament size')
+    size.add_argument('--lte', type=int, help='Maximum tournament size')
+
+    #max/min/eq date
+    tdate = parser.add_argument_group('Tournament date')
+    tdate.add_argument('--gte', type=str2timestamp, help='Minimum tournament date')
+    tdate.add_argument('--lte', type=str2timestamp, help='Maximum tournament date')
+
+    standing = parser.add_argument_group('Placement')
+    standing.add_argument('--lte', type=int, help='Maximum placement')
+    standing.add_argument('--gte', type=int, help='Minimum placement')'''
+
+
+    # Parse the command-line arguments
+
+
+    pass
 
